908e586d089faedb6326df939f51ae2b4e16b8d1
centralsolidariedade <central.solidariedade.project@gmail.com>
2024-05-28 16:37:35 -0300

    Update SimpleView.jsx

--- ong-project-main/frontend/src/components/selectView/SimpleView.jsx
+++ ong-project-main/frontend/src/components/selectView/SimpleView.jsx
@@ -1,11 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { UseID } from "../../main.jsx";
 import "../../routes/Home.css";
-import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { UseID } from '../../main.jsx';
-import '../../routes/Home.css'
-
 
 import "./SimpleView.css";
 
