ec72ccd120e13f6c72605fd8f96dfcf6932c7da8
centralsolidariedade <central.solidariedade.project@gmail.com>
2024-06-05 11:31:08 -0300

    :hammer: fix: link baseURL  deploy 2

--- ong-project-main/frontend/src/services/baseURL.js
+++ ong-project-main/frontend/src/services/baseURL.js
@@ -1,4 +1,4 @@
 // for local run
 // export const baseURL = "http://localhost:7777";
 
-export const baseURL = "https://ong-project-latest-3.onrender.com/";
+export const baseURL = "https://ong-project-latest-3.onrender.com";
